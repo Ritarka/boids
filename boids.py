30b5257ad63aed61f6f07a84a1b32fb27e89afcb
Ritarka <ritarkasamanta@gmail.com>
2024-02-10 09:48:30 -0700

    added boid shapes as well as better coalescing

--- boids.py
+++ boids.py
@@ -14,8 +14,8 @@ class Boid:
         self.vx = random.random() * 2 - 1
         self.vy = random.random() * 2 - 1
     
-margin = 40
-turnfactor = 0.02
+margin = 160
+turnfactor = 0.8
 visual_range = 50
 
 protect_range = 20
@@ -23,28 +23,56 @@ protect_range = 20
 centering_factor = 0.0005
 matching_factor = 0.005
 
-avoid_factor = 0.04
+avoid_factor = 0.08
 
 min_speed = 2
-max_speed = 6
+max_speed = 4
+
+def distance(a, b):
+    alpha = 0.9604
+    beta = 0.3978
+    
+    a = abs(a)
+    b = abs(b)
+    
+    return alpha * max(a, b) + beta * min(a, b)
+
+import math
+
+def rotate(origin, point, velocity):
+    
+    angle = math.atan2(velocity[1], velocity[0])
+    
+    ox, oy = origin
+    px, py = point
+    
+    dx = px - ox
+    dy = py - oy
+    
+    sin = math.sin(angle)
+    cos = math.cos(angle)
+
+    qx = ox + cos * dx - sin * dy
+    qy = oy + sin * dx + cos * dy
+    return qx, qy
     
 def update(boid):
     
-    if boid.y < margin and boid.vy < 0:
-        boid.vy += turnfactor * (margin - boid.y)
-    elif boid.y > 640 - margin and boid.vy > 0:
-        boid.vy -= turnfactor * (margin - boid.y + 640)
-    elif boid.x < margin and boid.vx < 0:
-        boid.vx += turnfactor * (margin - boid.x)
-    elif boid.x > 640 - margin and boid.vx > 0:
-        boid.vx -= turnfactor * (margin - boid.x + 640)
+    if boid.y < margin:
+        boid.vy += turnfactor
+    elif boid.y > 640 - margin:
+        boid.vy -= turnfactor
+    elif boid.x < margin:
+        boid.vx += turnfactor
+    elif boid.x > 640 - margin:
+        boid.vx -= turnfactor
     
     for other in boids:
         if other == boid:
             continue
         dx = other.x - boid.x
         dy = other.y - boid.y
-        dist = math.sqrt(pow(dx, 2) + pow(dy, 2))
+        dist = distance(dx, dy)
         
         neighbors = 0
         
@@ -81,7 +109,7 @@ def update(boid):
         boid.vy += close_y * avoid_factor    
     
             
-    speed = math.sqrt(pow(boid.vx, 2) + pow(boid.vy, 2))
+    speed = distance(boid.vx, boid.vy)
     if speed > max_speed:
         boid.vx = max_speed * boid.vx / speed
         boid.vy = max_speed * boid.vy / speed
@@ -93,16 +121,17 @@ def update(boid):
     boid.y += boid.vy
 
         
-        
+screen_x = 640
+screen_y = 640  
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((640, 640))
+screen = pygame.display.set_mode((screen_x, screen_y))
 clock = pygame.time.Clock()
 running = True
 
 for i in range(200):
-    boids.append(Boid(random.randint(0, 640), random.randint(0, 640)))
+    boids.append(Boid(random.randint(0, screen_x), random.randint(0, screen_y)))
 
 
 while running:
@@ -121,7 +150,11 @@ while running:
 
     # RENDER YOUR GAME HERE
     for boid in boids:
-        pygame.draw.rect(screen, "white", Rect(boid.x, boid.y, 10, 10))
+        # pygame.draw.rect(screen, "white", Rect(boid.x, boid.y, 10, 10))
+        points = [(boid.x + 6.66, boid.y), (boid.x - 3.33, boid.y - 5), (boid.x - 3.33, boid.y + 5)]
+        points = [rotate((boid.x, boid.y), point, (boid.vx, boid.vy)) for point in points]
+        pygame.draw.polygon(screen, "white", points)
+
         update(boid)
 
     # flip() the display to put your work on screen
